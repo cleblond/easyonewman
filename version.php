72d42f3268ed35dfbff40b7e769fd03895df19a3
Carl LeBlond <carl.leblond@iup.edu>
2014-06-13 10:49:59 -0400

    update version

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
  */
 
 defined('MOODLE_INTERNAL') || die();
-$plugin->version   = 2014041500;
+$plugin->version   = 2014061300;
 $plugin->requires  = 2012062500;
 $plugin->cron      = 0;
 $plugin->component = 'qtype_easyonewman';
